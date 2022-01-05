e87d2e70e14868343299374988ab6c95e2e2e1f0
Антон Довиденков <79474789+Yogel9@users.noreply.github.com>
2022-01-05 20:21:43 +0400

    Проверка работы библиотек на данных о продаже шампуня

--- main.py
+++ main.py
@@ -3,49 +3,20 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import tensorflow as tf #библиотека для машинного обучения
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pandas as pd #библиотека над numpy
-
-
-
-def univariate_data(dataset, start_index, end_index, history_size, target_size):
-  data = []
-  labels = []
-
-  start_index = start_index + history_size
-  if end_index is None:
-    end_index = len(dataset) - target_size
-
-  for i in range(start_index, end_index):
-    indices = range(i-history_size, i)
-    # Reshape data from (history_size,) to (history_size, 1)
-    data.append(np.reshape(dataset[indices], (history_size, 1)))
-    labels.append(dataset[i+target_size])
-  return np.array(data), np.array(labels)
-
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    mpl.rcParams['figure.figsize'] = (8, 6)
-    mpl.rcParams['axes.grid'] = False
-    zip_path = tf.keras.utils.get_file(
-        origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
-        fname='jena_climate_2009_2016.csv.zip',
-        extract=True)
-    csv_path, _ = os.path.splitext(zip_path)
-    df = pd.read_csv(csv_path)
-    df.head()
-    TRAIN_SPLIT = 300000
-    tf.random.set_seed(13)
-    uni_data = df['T (degC)']
-    uni_data.index = df['Date Time']
-    uni_data.head()
-    uni_data.plot(subplots=True)
-    uni_data = uni_data.values
+    # load dataset
+    def parser(x):
+        return datetime.strptime('190' + x, '%Y-%m')
+    series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+    # summarize first few rows
+    print(series.head())
+    # line plot
+    series.plot()
+    pyplot.show()
 
 
